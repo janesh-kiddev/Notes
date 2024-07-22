028cc57b7e5c1930c61a5d3dd96a8474fdf35cfb
janesh-kiddev <officialjanesh12@gmail.com>
2024-07-22 20:36:37 +0530

    commit changes

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=['GET','POST'])
 def login():
     return render_template("login.html",boolean=False)
 
@@ -10,6 +10,24 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup',methods=['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstNme')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash("Email must be greater than 3 characters",category='error')
+        elif len(firstname) < 2:
+            flash("FirstName must be greater than 1 characters",category='error')
+        elif password1 != password2:
+            flash("password must be equal to the confirmed password",category='error')
+        elif len(password1) < 7:
+            flash("The password is too short must be greater than 7",category='error')
+        else:
+            flash('Account Created',category='Success')
+
+
     return render_template("signup.html")
